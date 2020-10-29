a936202f35c3e8c24e5a97369fa5755252143ae0
nikolaevnikita <nikolaevnikita@mail.ru>
2020-10-29 14:11:50 +0300

    Initial Commit

---
+++ MyCarsCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCarsCoreData
+//
+//  Created by Николаев Никита on 29.10.2020.
+//  Copyright © 2020 Николаев Никита. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
